a2bc377c4d6ddda64ffc516752f7fc6ab5c14c1a
Palak <behlpari4@gmail.com>
2026-02-19 00:10:31 +0530

    Setup Express application with security middleware

---
+++ server/express.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template'
+
+const CURRENT_WORKING_DIR = process.cwd()
+
+const app = express()
+
+// Parse body params and attach them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// Secure apps by setting various HTTP headers
+app.use(helmet())
+// Enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  } else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+  }
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
+export default app
